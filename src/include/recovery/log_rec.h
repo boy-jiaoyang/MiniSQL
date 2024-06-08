b3c763c7571d45905c1286e5921d1aa3c691b581
CaramelteaLatte <cyx2691264332@gmail.com>
2024-06-08 21:40:04 +0800

    complement part 6

--- src/include/recovery/log_rec.h
+++ src/include/recovery/log_rec.h
@@ -32,6 +32,12 @@ struct LogRec {
     lsn_t lsn_{INVALID_LSN};
     lsn_t prev_lsn_{INVALID_LSN};
 
+    [[maybe_unused]] KeyType old_key;
+    [[maybe_unused]] ValType old_value;
+    [[maybe_unused]] KeyType new_key;
+    [[maybe_unused]] ValType new_value;
+    txn_id_t txn_id;
+
     /* used for testing only */
     static std::unordered_map<txn_id_t, lsn_t> prev_lsn_map_;
     static lsn_t next_lsn_;
@@ -46,42 +52,98 @@ typedef std::shared_ptr<LogRec> LogRecPtr;
  * TODO: Student Implement
  */
 static LogRecPtr CreateInsertLog(txn_id_t txn_id, KeyType ins_key, ValType ins_val) {
-    return nullptr;
+  LogRec res ;
+  res.type_ = LogRecType::kInsert;
+  res.txn_id = txn_id;
+  auto id = LogRec::prev_lsn_map_.at(txn_id);
+  res.lsn_ = LogRec::next_lsn_;
+  res.prev_lsn_ = id;
+  LogRec::prev_lsn_map_[txn_id] = LogRec::next_lsn_;
+  ++LogRec::next_lsn_;
+  res.new_key = ins_key;
+  res.new_value = ins_val;
+  return std::make_shared<LogRec>(res);
 }
 
 /**
  * TODO: Student Implement
  */
 static LogRecPtr CreateDeleteLog(txn_id_t txn_id, KeyType del_key, ValType del_val) {
-    return nullptr;
+  LogRec res ;
+  res.type_ = LogRecType::kDelete;
+  res.txn_id = txn_id;
+  auto id = LogRec::prev_lsn_map_.at(txn_id);
+  res.lsn_ = LogRec::next_lsn_;
+  res.prev_lsn_ = id;
+  LogRec::prev_lsn_map_[txn_id] = LogRec::next_lsn_;
+  ++LogRec::next_lsn_;
+  res.new_key = del_key;
+  res.new_value = del_val;
+  LogRec::prev_lsn_map_[txn_id] = res.lsn_;
+  return std::make_shared<LogRec>(res);
 }
 
 /**
  * TODO: Student Implement
  */
 static LogRecPtr CreateUpdateLog(txn_id_t txn_id, KeyType old_key, ValType old_val, KeyType new_key, ValType new_val) {
-    return nullptr;
+  LogRec res ;
+  res.type_ = LogRecType::kUpdate;
+  res.txn_id = txn_id;
+  auto id = LogRec::prev_lsn_map_.at(txn_id);
+  res.lsn_ = LogRec::next_lsn_;
+  res.prev_lsn_ = id;
+  LogRec::prev_lsn_map_[txn_id] = LogRec::next_lsn_;
+  ++LogRec::next_lsn_;
+  res.old_key = old_key;
+  res.old_value = old_val;
+  res.new_key = new_key;
+  res.new_value = new_val;
+  LogRec::prev_lsn_map_[txn_id] = res.lsn_;
+  return std::make_shared<LogRec>(res);
 }
 
 /**
  * TODO: Student Implement
  */
 static LogRecPtr CreateBeginLog(txn_id_t txn_id) {
-    return nullptr;
+ LogRec res;
+ res.type_ = LogRecType::kBegin;
+ res.txn_id = txn_id;
+ res.lsn_ = LogRec::next_lsn_;
+ res.prev_lsn_ = INVALID_LSN;
+ LogRec::prev_lsn_map_[txn_id] = LogRec::next_lsn_;
+ ++LogRec::next_lsn_;
+ return std::make_shared<LogRec>(res);
 }
-
 /**
  * TODO: Student Implement
  */
 static LogRecPtr CreateCommitLog(txn_id_t txn_id) {
-    return nullptr;
+ LogRec res ;
+ res.type_ = LogRecType::kCommit;
+ res.txn_id = txn_id;
+ auto id = LogRec::prev_lsn_map_.at(txn_id);
+ res.lsn_ = LogRec::next_lsn_;
+ res.prev_lsn_ = id;
+ LogRec::prev_lsn_map_[txn_id] = LogRec::next_lsn_;
+ ++LogRec::next_lsn_;
+ return std::make_shared<LogRec>(res);
 }
 
 /**
  * TODO: Student Implement
  */
 static LogRecPtr CreateAbortLog(txn_id_t txn_id) {
-    return nullptr;
+ LogRec res ;
+ res.type_ = LogRecType::kAbort;
+ res.txn_id = txn_id;
+ auto id = LogRec::prev_lsn_map_.at(txn_id);
+ res.lsn_ = LogRec::next_lsn_;
+ res.prev_lsn_ = id;
+ LogRec::prev_lsn_map_[txn_id] = LogRec::next_lsn_;
+ ++LogRec::next_lsn_;
+ return std::make_shared<LogRec>(res);
 }
 
 #endif  // MINISQL_LOG_REC_H
--- src/include/recovery/recovery_manager.h
+++ src/include/recovery/recovery_manager.h
@@ -25,17 +25,85 @@ public:
     /**
     * TODO: Student Implement
     */
-    void Init(CheckPoint &last_checkpoint) {}
+    void Init(CheckPoint &last_checkpoint) {
+        data_ = last_checkpoint.persist_data_;
+        active_txns_ = last_checkpoint.active_txns_;
+        persist_lsn_ = last_checkpoint.checkpoint_lsn_;
+    }
 
     /**
     * TODO: Student Implement
     */
-    void RedoPhase() {}
+    void RedoPhase() {//重做从时间点开始的日志记录
+        //从最后一次检查点的日志序列号（的下一个开始，遍历所有日志记录到当前的日志序列号
+        for(int i = persist_lsn_; i < LogRec::next_lsn_; i++) {
+            auto log = log_recs_.at(i);
+            active_txns_[log->txn_id] = log->lsn_;
+            switch (log->type_) {
+                case LogRecType::kInsert:
+                    data_[log->new_key] = log->new_value;
+                break;
+                case LogRecType::kDelete:
+                    data_.erase(log->new_key);
+                break;
+                case LogRecType::kUpdate:
+                    data_.erase(log->old_key);
+                    data_[log->new_key] = log->new_value;
+                break;
+                case LogRecType::kCommit:
+                    active_txns_.erase(log->lsn_);
+                break;
+                case LogRecType::kAbort:
+                    auto t_log = log;
+                while(t_log->prev_lsn_ != INVALID_LSN){
+                    auto t = t_log->prev_lsn_;
+                    t_log = log_recs_.at(t);
+                    if(t_log->type_ == LogRecType::kInsert){
+                        data_.erase(t_log->new_key);
+                    }else if(t_log->type_ == LogRecType::kDelete){
+                        data_[t_log->new_key] = t_log->new_value;
+                    }else if(t_log->type_ == LogRecType::kUpdate){
+                        data_.erase(t_log->new_key);
+                        data_[t_log->old_key] = t_log->old_value;
+                    }
+                }
+                break;
+            }
+        }
+
+
+    }
 
     /**
     * TODO: Student Implement
     */
-    void UndoPhase() {}
+    void UndoPhase() {
+        for(const auto &txn : active_txns_) {
+            auto final_res = log_recs_.at(txn.second);
+            if(final_res->type_ != LogRecType::kCommit) {
+                auto log = final_res;
+                while(log->prev_lsn_ != INVALID_LSN) {
+                    log = log_recs_.at(log->prev_lsn_);
+                    switch (log->type_) {
+                        case LogRecType::kInsert:
+                            data_.erase(log->new_key);
+                        break;
+                        case LogRecType::kDelete:
+                            data_[log->new_key] = log->new_value;
+                        break;
+                        case LogRecType::kUpdate:
+                            data_.erase(log->new_key);
+                        data_[log->old_key] = log->old_value;
+                        break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+    }
+
+
 
     // used for test only
     void AppendLogRec(LogRecPtr log_rec) { log_recs_.emplace(log_rec->lsn_, log_rec); }
